26388dc64605b2e2706e3a74007aa47a77b4d564
Алексей Зотов <93433900+8merka@users.noreply.github.com>
2023-12-25 21:56:17 +0300

    del: remove accidently uploadet directory

--- pages/AddHousePage/handleTypeChange.js
+++
@@ -1,4 +0,0 @@
-export const handleTypeChange = (event, setType) => {
-    setType(event.target.value);
-};
-
--- pages/LogInPage/handleRegClick.js
+++
@@ -1,3 +0,0 @@
-export async function handleRegClick(navigate) {
-    navigate("/reg");
-}
